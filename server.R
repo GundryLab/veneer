c5f8b4d67251bc402910e120528f6bb8a207452d
Jack Littrell <jlittrell@mcw.edu>
2023-11-11 09:18:47 -0600

    Accidentally did not incude server.R in the last commit.

--- server.R
+++ server.R
@@ -13,7 +13,7 @@ library(ggplot2)
 library(ggrepel)
 
 
-# options(shiny.maxRequestSize = 150*1024^2)
+options(shiny.maxRequestSize = 150*1024^2)
 source('functions.R')
 
 ###############################################################################
@@ -66,7 +66,7 @@ shinyServer(function(input, output, session) {
     }
     for (i in seq_along(files)) {
       withProgress(message = paste0('Reading and Processing ', files[i]), value = 0, {
-        print(files[i])
+#        print(files[i])
         parts <- strsplit(files[i], "\\.")[[1]]
 #        print(parts)
         ext <- parts[length(parts)]
@@ -108,12 +108,9 @@ shinyServer(function(input, output, session) {
         # spreadsheet to save memory.  I will need to put the information back into the 
         # PSM tabs in the output spreadsheet.  The ID column is used for this purpose
         p <- cScIFTING( df[c('ID', 'Master Protein Accessions', 'Annotated Sequence')] )
-#        nG <- p[[11]]
         nG <-sum(p[[1]]['countNG'])
         tot <-sum(p[[1]]['PSMwSCM'])
-#        print(paste(nG, nG1, tot))
-#        p[length(p)] <- NULL
-        
+
         # add a column displaying the % of SCMs that are nG 
         # simply divide nG column by SCM column
         p[[1]]['pctNG'] <- round(p[[1]]['countNG']/p[[1]]['PSMwSCM'], 2)
